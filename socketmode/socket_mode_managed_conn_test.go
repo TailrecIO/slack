312bcaa2ff20b24ae08de246a3ec779075a0fe76
Hussachai Puripunpinyo <hussachai@gmail.com>
2022-12-19 12:07:10 -0800

    fix import statement

--- socketmode/socket_mode_managed_conn_test.go
+++ socketmode/socket_mode_managed_conn_test.go
@@ -9,8 +9,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/slack-go/slack"
-	"github.com/slack-go/slack/slacktest"
+	"github.com/TailrecIO/slack"
+	"github.com/TailrecIO/slack/slacktest"
 
 	"github.com/stretchr/testify/assert"
 )
